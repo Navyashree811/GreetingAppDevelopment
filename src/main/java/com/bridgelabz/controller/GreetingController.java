3d39511ab4f7b775d0c90475f9d6035ee76d9954
hp-pc <navyashree811@gmail.com>
2022-10-13 13:01:40 +0530

    Added : add

--- src/main/java/com/bridgelabz/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -10,30 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 	private static final String template = "Hello. %s!";
-	private final AtomicLong counter = new AtomicLong();
-
+	private  final AtomicLong counter  = new AtomicLong();
+	
 	/*
-	 * curl localhost:8080/greeting
-	 * 
-	 * @return={id =1 , content="hello world!}
-	 * curllocalhost:8080/greeting?name=Navya
-	 * 
-	 * @return= { id=2, content="hello Navya!
-	 */
+     *curl localhost:8080/greeting
+     @return={id =1 , content="hello world!}
+     * curllocalhost:8080/greeting?name=Navya
+     * @return= { id=2, content="hello Navya!
+     */
 
-	@GetMapping(value = { "/greeting", "/greeting/", "/greeting/home" })
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
-
-	/*
-	 * curl localhost:8080/greeting/Navya
-	 * 
-	 * @return={id =1 , content="hello Navya!}
-	 */
-	@GetMapping("greeting/{name}")
-	public Greeting greetings(@PathVariable String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+	 @GetMapping(value={"/greeting","/greeting/","/greeting/home"})
+	    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	    }
+	    /*
+	    *curl localhost:8080/greeting/Navya
+	    @return={id =1 , content="hello Navya!}
+	     */
+	    @GetMapping("greeting/{name}")
+	    public Greeting greetings(@PathVariable String name) {
+	        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	    }
 
 }
